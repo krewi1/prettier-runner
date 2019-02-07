d50930e93374b4214b351e9aacd8fdbcb28755e8
p.kratochvil <p.kratochvil@gmc.net>
2019-02-07 08:50:06 +0100

    prettier

--- test/prettier/specs/tsLintFormatter.ts
+++ test/prettier/specs/tsLintFormatter.ts
@@ -11,69 +11,13 @@ assert.expose(assertion, { prefix: "" });
 const readFileAsync = promisify(readFile);
 const writeFileAsync = promisify(writeFile);
 const unlinkFileAsync = promisify(unlink);
-import {TsLint} from "../../../src/formatters/typescriptFormatter";
+import { TsLint } from "../../../src/formatters/typescriptFormatter";
 
 const dirname: string = __dirname;
 const packageJsonPath: string = join(dirname, "package.json");
 const jsName: string = "test.ts";
 // const tsLintFile: string = "tslint.json";
 const jsPath: string = join(dirname, jsName);
-
-/*describe("tslint runner", () => {
-    let sandBox: SinonSandbox;
-    let exec: SinonStub;
-
-    beforeEach(async () => {
-        await writeFileAsync(
-            packageJsonPath,
-            `{
-            "printWidth": 140,
-            "tabWidth": 4,
-            "parser": "typescript"
-        }`
-        );
-        sandBox = createSandbox();
-        exec = sandBox.stub(process, "exec");
-
-        await writeFileAsync(
-            jsPath,
-            `import {a} from "a";
-
-function f() {
-    let v = "ahoj";
-    console.log(v)
-    return a;
-}`
-        );
-    });
-
-    describe("files are provided from external source", () => {
-        it("then format them with tslint autofixer", async () => {
-            // given
-            exec.yields(undefined, jsName);
-            const iface = setup({
-                prettierCfgPath: packageJsonPath,
-                projectRoot: __dirname,
-                changedPaths: ["test/prettier/specs/test.ts"],
-                tslintCfgPath: tsLintPath
-            });
-
-            // when
-            await iface.tsLintCheck();
-
-            // then
-            const result = await readFileAsync(jsPath);
-            expect(result.toString()).to.equal(`import { a } from "a";
-
-function f() {
-    const v = "ahoj";
-    console.log(v)
-    return a;
-}
-`);
-        });
-    });
-});*/
 
 describe("Tslint Formatter", () => {
     let sandBox: SinonSandbox;
